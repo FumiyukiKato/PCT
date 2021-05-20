6cfcc1b6ecf6b87f72f5fe6e23a72ed989abc931
FumiyukiKato <fumilemon79@gmail.com>
2021-05-20 20:08:06 +0900

    update generate script

--- tools/trajectory/generate.py
+++ tools/trajectory/generate.py
@@ -5,11 +5,13 @@ import geopandas as gpd
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+import os
 
 
 parser = argparse.ArgumentParser(description='Generate individual human mobility data by extended DensityEPR (see scimob https://github.com/scikit-mobility/scikit-mobility .)')
 parser.add_argument('--target', type=str, help="target for dataset, server | client")
 parser.add_argument('--size', type=int, help="data size")
+parser.add_argument('--dir', type=str, help="directory name")
 args = parser.parse_args()
 
 
@@ -39,7 +41,7 @@ def gauusian_bridge(tdf, uid, random_state, minutes):
     table_size = len(tdf)
     for i in range(-1, table_size):
         if (i == table_size - 1):
-            time_diff = (int(end_time.timestamp()) - tdf['time'].iloc[i])
+            time_diff = int(end_time.timestamp()) - tdf['time'].iloc[i]
             curr_time = tdf['time'].iloc[i]
             i -= 10
         elif (i == -1):
@@ -47,10 +49,13 @@ def gauusian_bridge(tdf, uid, random_state, minutes):
             curr_time = int(true_start_time.timestamp())
             i += 10
         else:
-            time_diff = (tdf['time'].iloc[i+1] - tdf['time'].iloc[i])
+            time_diff = tdf['time'].iloc[i+1] - tdf['time'].iloc[i]
             curr_time = tdf['time'].iloc[i]
-        
+    
         time_times = time_diff // unix_minutes
+        if time_times == 0:
+            continue
+
         lat_diff = (tdf['lat'].iloc[i+1] - tdf['lat'].iloc[i]) / time_times
         lng_diff = (tdf['lng'].iloc[i+1] - tdf['lng'].iloc[i]) / time_times
         
@@ -83,7 +88,7 @@ def for_all_user(tdf, random_state, minutes=10):
         df_list.append(gauusian_bridge(tdf[tdf['uid'] == uid], uid, random_state, minutes))
     return pd.concat(df_list, axis=0)
 
-def generate_server_data(agents=100, seed=1, minutes=10):
+def generate_server_data(agents=100, seed=1, minutes=10, dir="data"):
     state = np.random.RandomState(seed)
     start_locations = list(state.choice(list(range(0,62)), agents, True))
     depr = ConstantTimeDensityEPR()
@@ -95,32 +100,34 @@ def generate_server_data(agents=100, seed=1, minutes=10):
     tdf['time'] = tdf['datetime'].apply(lambda x: int(x.timestamp()))
     tdf['time'] = apply_minute(tdf, minutes=minutes)
     tdf = for_all_user(tdf, state, minutes)
-    tdf[['time', 'lat', 'lng']].to_csv(f'NY-DensityEPR-{minutes}-{seed}-{agents}.csv', index=False)
+    tdf[['time', 'lat', 'lng']].to_csv(f'{dir}/NY-DensityEPR-{minutes}-{seed}-{agents}.csv', index=False)
 
-def generate_client_data(client_size, seed=1, minutes=10):
+def generate_client_data(client_size, seed=1, minutes=10, dir="data"):
     agents = 1
     state = np.random.RandomState(seed)
     
     for i in tqdm(range(client_size)):
+        gen_seed = state.randint(100000000)
         start_locations = list(state.choice(list(range(0,62)), agents, True))
         depr = ConstantTimeDensityEPR()
         # start the simulation
-        tdf = depr.generate(start_time, end_time, tessellation, relevance_column='population', n_agents=agents, random_state=seed, show_progress=True, starting_locations=start_locations)
+        tdf = depr.generate(start_time, end_time, tessellation, relevance_column='population', n_agents=agents, random_state=gen_seed, show_progress=True, starting_locations=start_locations)
     
         tdf = tdf[tdf['datetime'] >= true_start_time]
         tdf['time'] = tdf['datetime'].apply(lambda x: int(x.timestamp()))
         tdf['time'] = apply_minute(tdf, minutes=minutes)
         tdf = for_all_user(tdf, state, minutes)
-        tdf[['time', 'lat', 'lng']].to_csv(f'client/client-{i}-NY-DensityEPR-{minutes}-{seed}-{agents}.csv', index=False)
+        tdf[['time', 'lat', 'lng']].to_csv(f'{dir}/client/client-{i}-NY-DensityEPR-{minutes}-{seed}-{agents}.csv', index=False)
 
         
 if __name__ == '__main__':
     minutes = 10
     seed = 1
+    os.makedirs(args.dir, exist_ok=True)
     if args.target == "server":
-        generate_server_data(agents=args.size, seed=seed, minutes=minutes)
+        generate_server_data(agents=args.size, seed=seed, minutes=minutes, dir=args.dir)
     elif args.target == "client":
-        generate_client_data(client_size=args.size, seed=seed, minutes=minutes)
+        generate_client_data(client_size=args.size, seed=seed, minutes=minutes, dir=args.dir)
     else:
         assert(False, "invalid parameter target")
 
